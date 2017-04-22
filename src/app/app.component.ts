b5b7050191e509d547ed75e37c245758fa7625d5
Nicholas Rupe <nrupe84@gmail.com>
2017-04-22 13:39:45 -0400

    Bootstrap in CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../assets/css/app.component.css', '../assets/css/bootstrap.css']
 })
 export class AppComponent {
   title = 'app works!';
